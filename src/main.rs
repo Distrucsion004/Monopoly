e3c8317cced0acfe1b6664578089826d4bb77f04
Distrucsion004 <razvan.curpan@gmail.com>
2023-01-26 16:32:42 +0200

    Figured out how to transfer data from module to main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
+use std::borrow::Borrow;
+
 use rand::Rng;
-use std::collections::HashMap;
+
+use crate::player::update_railroads;
 mod data;
+mod player;
 fn main() {
-    let board = data::Props();
+    let things = data::props();
+    // stores name - type pairs
+    let board = things.1;
+    //stores indexed names;
+    let names = things.0;
+    //store arrays of types
+    let mut props = things.2;
+    let mut railroads = things.3;
+    let mut utilities = things.4;
+    let mut taxes = things.5;
+    let mut chances = things.6;
+    let mut chests = things.7;
+    let mut specials = things.8;
     let mut dice = data::Dice{
         d1 : rand::thread_rng().gen_range(1..7),
         d2 : rand::thread_rng().gen_range(1..7)
     };
-    //println!("{:?}", board.5[0].boardposition);
-    for i in 0..41{
-        for j in board{
-            for n in j{
-            println!("{:?}", n.boardposition);
-            }
-        }
-    }
-    //println!("{}", dice.roll().d1);
+    let mut player = player::initialize_player();
+    player.add_money(10);
+    println!("{}", player.money);
+    player.take_money(40);
+    println!("{}", player.money);
+    player.dice_move(2);
+    println!("{}", player.boardposition);
+    player.add_railroad(names[railroads[0].boardposition as usize].to_string());
+    update_railroads(&mut railroads[0]);
+    println!("{:?}", player.railroads);
+    println!("{}", railroads[0].owned);
+
 }
+    //println!("{}", dice.roll().d1);
+//}
 
-
+/*fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}*/
