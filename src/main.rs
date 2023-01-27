db3b2bb9002a3690dd302bc075a7a71984384053
Distrucsion004 <razvan.curpan@gmail.com>
2023-01-27 12:29:09 +0200

    Implemented a lot more functions and added a help prompt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,62 @@
 use player::{Player, initialize_player};
 use rand::Rng;
 use std::{env, ops::Index, collections::HashMap};
-use crate::player::{update_ownership};
+use crate::{player::{update_ownership}, data::space_type};
 mod data;
 use data::{Space};
+use std::io;
 mod player;
 fn main() {
+    
+    let args: Vec<String> = env::args().collect();
+    let player_count = &args[1];
+    let count = player_count.parse::<i32>().unwrap();
+    println!("{}", count);
+    let mut playe_r:Vec<Player> = Vec::new();
+    for player in 0..count{
+        playe_r.push(initialize_player());
+    }
+    
+    let mut spaces = data::props();
+    let mut dice = data::Dice{
+        d1 : rand::thread_rng().gen_range(1..7),
+        d2 : rand::thread_rng().gen_range(1..7),
+
+    };
+    println!("Enter \"help\" for a list of commands");
+    loop{
+    for mut i in playe_r{
+    loop{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        //input = input.trim();
+        match input.trim(){
+            "help" => list_commands(),
+            "roll" => {dice.roll();
+                println!("{}, {}", &dice.d1, &dice.d2);},
+            "move" => { i.dice_move(dice.total());
+                        println!("You landed on {}",spaces[i.boardposition as usize].name)},
+            "check"=> {println!("{}",spaces[i.boardposition as usize].name)},
+            "buy" =>  {if ( spaces[i.boardposition as usize].kind == space_type::Prop ||
+                            spaces[i.boardposition as usize].kind == space_type::Utility ||
+                            spaces[i.boardposition as usize].kind == space_type::Railroad) && 
+                            spaces[i.boardposition as usize].owned == false{
+                        i.buy_prop(&mut spaces[i.boardposition as usize]);
+                        }
+                        else if spaces[i.boardposition as usize].owned == true{
+                            println!("Space is already owned!");
+                        }
+                        else{println!("Not a buyable field");}
+               }
+            "owned" => {println!("props = {:?},\n railroads = {:?},\n utilities = {:?}\n", i.props, i.railroads, i.utilities);}
+            "end_turn" => {break},
+            
+            _ => (),
+
+        }
+    }
+}
+}
     /*
     let args: Vec<String> = env::args().collect();
     let player_count = &args[1];
@@ -20,17 +71,8 @@ fn main() {
     for i in playe_r{
         println!("{:?}", i);
     }*/
-    
-    let mut spaces = data::props();
-    
-    
-    let mut dice = data::Dice{
-        d1 : rand::thread_rng().gen_range(1..7),
-        d2 : rand::thread_rng().gen_range(1..7)
-    };
-
     //debugging code 
-   /*
+    /*
     let mut player = player::initialize_player();
     player.add_money(10);
     println!("{}", player.money);
@@ -55,4 +97,18 @@ fn check_space_type(u : &Player, nam : &[&str; 40], boa: &HashMap<String, String
     return r;
 }
 
-
+fn list_commands()->(){
+    print!("
+                \"check\"     - check position on board
+                \"roll \"     - rolls dice
+                \"move\"      - move as many spaces as the dice show
+                \"owned\"     - see owned fields
+                \"buy\"       - buy the space you are located on
+                \"buy_house\" - add a house to a property
+                \"buy_hotel\" - add a hotel to a property
+                \"loan\"      - take a loan
+                \"trade_prop\"- trade one of your property
+                \"mortgage\"  - get a mortgage on one of your properties
+                \"end_turn\"  - end your turn
+            " );
+}
